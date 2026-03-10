3aaa04348c42a1c281ea10d4cf97a526cb58fab8
rtupakul <rtupakul@cisco.com>
2026-03-10 11:00:55 -0400

    CSCwt27786: Create local placeholder dir when

--- csi/volumeutils.py
+++ csi/volumeutils.py
@@ -1127,6 +1127,8 @@ def handle_external_volume(volume, mountpoint, is_client, hosts):
         with mount_lock:
             # Re-check inside lock to prevent TOCTOU race (FUSE mount leak)
             if is_gluster_mount_proc_running(volname, mountpoint):
+                if not os.path.exists(mountpoint):
+                    makedirs(mountpoint)
                 logging.debug(logf(
                     "Already mounted (inside lock)",
                     mount=mountpoint
@@ -1138,6 +1140,8 @@ def handle_external_volume(volume, mountpoint, is_client, hosts):
                                       volume['g_options'],
                                       is_client)
     else:
+        if not os.path.exists(mountpoint):
+            makedirs(mountpoint)
         logging.debug(logf(
             "Already mounted",
             mount=mountpoint
